e1082a376e59d4351592196ccd4de395d7afd201
indhuja05jeganathan-ai <indhuja05jeganathan@gmail.com>
2026-03-12 00:39:38 +0530

    Remove commented-out main method in Remove White Spaces.java

---
+++ Remove White Spaces.java
@@ -0,0 +1,10 @@
+public class see {
+//     public static void main(String[] args) {
+
+//         String str = "hello world";
+
+//         str = str.replace(" ", "");
+
+//         System.out.println(str);
+//     }
+// }
