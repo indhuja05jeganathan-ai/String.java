cc57b7ef560184fccd013fe2ece5f6f54450bc7e
indhuja05jeganathan-ai <indhuja05jeganathan@gmail.com>
2026-03-12 00:38:27 +0530

    Add SubStr.java with commented main method

---
+++ SubStr.java
@@ -0,0 +1,13 @@
+public class see {
+//     public static void main(String[] args) {
+
+//         String mainStr = "hello";
+//         String subStr = "ll";
+
+//         if (mainStr.contains(subStr)) {
+//             System.out.println("Substring found");
+//         } else {
+//             System.out.println("Substring not found");
+//         }
+//     }
+// }
